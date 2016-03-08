1101a7ca3bc481a7894db35ce25d42f3a3c4e7e8
will_is_coding <william@ingold.org>
2016-03-08 23:22:13 +0000

    Cleanup

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,21 @@
 'use strict';
+var multer = require('multer');
+var upload = multer( { dest: 'uploads/' });
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	app.post('/api/fileanalyze', upload.single('upl'), function(req, res) {
+		res.writeHead( 200, {
+			"Content-Type": "text/html"
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+		res.write('Your file <b>' + req.file.originalname + '</b> is <b>' + req.file.size + '</b> bytes.');
+		res.write('<br> Return <a href="https://freecodecamp-metadata-microservice-will-is-coding.c9users.io">home</a> to check another file\'s size');
+		res.end();
+	});
 };
